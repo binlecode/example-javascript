19add70a644a8baf73f18307dc7990b53aaaf2de
Bin Le <bin.le.code@gmail.com>
2020-08-19 14:59:07 -0400

    add scope example for arrow funciton

--- src/arrow-function.js
+++ src/arrow-function.js
@@ -17,5 +17,38 @@ console.log(pwr(3));
 const pwr2 = (x) => x * x;
 console.log(pwr2(3));
 
+/**
+ * The handling of `this` is different in arrow functions compared to regular 
+ * functions. In short, with arrow functions there are no binding of this.
+ * In regular functions `this` represented the object that called the function.
+ * With arrow functions `this` keyword always represents the object that defined 
+ * the arrow function.
+ */
 
+function showFunThis() {
+    console.log("function's this is: " + this);
+}
+
+const showArrThis = () => { console.log("arrow funciton's this is: " + this) };
+
+showFunThis(); // undefined, since it is called in global
+showArrThis(); // [object Object], the reference of the script object
+
+// The example below has an arrow function defined by a scope including
+// a variable `ctx`, which is changed by the arrow funciton during its call.
+// Note that this arrow function is called within the scope of a regular
+// funciton, manageCtx, which is not aware of the ctx variable.
+
+let ctx = 'this is global ctx';
+const workWithCtx = (msg) => { 
+    console.log("arrow function gets context value: " + ctx);
+    ctx += ' - appended with: ' + msg;
+};
+
+function manageCtx(appendMsg) {
+    workWithCtx(appendMsg);
+}
+
+manageCtx("hello arrow function!");
+console.log("now the global ctx is: " + ctx);  // ctx is changed with appended msg
 
