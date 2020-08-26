0644efc1d5c364e7ffb93848a232000602887fe2
Bin Le <binle@Mbp16.local>
2020-08-25 21:28:56 -0400

    more examples

--- src/hello-js.js
+++ src/hello-js.js
@@ -1,2 +1,11 @@
 
-console.log('hello javascript');
+const dt = new Date();
+
+
+// string interpolation, supports multiple lines
+const message = 
+`Hello javascript, now is ${dt}.
+And this is a second line`;
+
+console.log(message);
+
---
+++ src/typeof-null.js
@@ -0,0 +1,24 @@
+
+// null
+//
+// in javascript, null is of type 'object', this is generally regarded as a bad language design
+console.log(typeof null); // => object
+// null represents an empty or non-existing value, but it must be assigned
+let a = null;
+console.log(a == undefined);  // => true, javascript double-equal does type coersion
+console.log(a === undefined); // => false
+
+// undefined
+//
+// undefined means a variable that is declared but not defined
+let x;
+console.log(typeof x);  // => undefined
+// undefined can be assigned, but usually not explicitly coded this way...
+x = undefined;
+console.log(typeof x);  // => undefined
+
+// typeof operator
+// the list of possible returned values are: 
+// number, string, boolean, function, object, and undefined
+// note that 'object' will be returned for null, array, and any other javascript object.
+// note that 'undefined' is only returned for undefined variable
