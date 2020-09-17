6252feda3ff8dd837cc69659e3da24a87edd0c9f
Bin Le <binle@Mbp16.local>
2020-09-16 23:27:02 -0400

    better comment

--- src/for-loop-old-school.js
+++ src/for-loop-old-school.js
@@ -11,12 +11,13 @@ for (let i = 0; i < a.length; i++) {
 
 // but the above is not efficlient as .lenth is evaluated for each iteration
 // here is a slightly better version where a.length is only evaluated once
+// this saving becomes bigger when array is long
 
 for (let i = 0, len = a.length; i < len; i++) {
     console.log(`${i} : ${a[i]}`);
 }
 
-// and there is the efficient, and simplified version
+// and here is an efficient, and simplified version
 // note that the loop ends on item becomes undefined when i goes outof index boundary
 
 for (let i = 0, item; item = a[i++];) {
