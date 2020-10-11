725868e58e30e7a57a5d8d64cf4c756dc69524de
Bin Le <bin.le.code@gmail.com>
2020-10-11 13:53:24 -0400

    add http server example - wip

---
+++ src/http-server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const url = require('url')
+/*
+http.createServer takes a function, known as the RequestListener with two
+parameters: request, and response.
+
+*/
+http.createServer((req, res) => {
+
+    const query = url.parse(req.url, true).query;
+    console.log(query);
+
+    res.end(`hello ${query.a}`);
+}).listen(8214);
+
+console.log('server running on port 8214');
+
+
