ca0c38d9b45da9a039c1586ba56d9a8ad584e930
Bin Le <bin.le.code@gmail.com>
2021-01-20 18:16:38 -0500

    revise class example

--- src/class-function.js
+++ src/class-function.js
@@ -4,6 +4,7 @@
 'use strict';
 
 // javascript class is function by nature
+// funcitons are objects, and inheritance is handled through the prototype
 
 class Foo {}
 console.log(Foo);  // [Function: Foo]
@@ -11,12 +12,23 @@ console.log(Foo);  // [Function: Foo]
 // A constructor is called when an instance is created using the new keyword.
 
 class Bar {
-    constructor({prop1, prop2='value2'}) {
-        this.p1 = prop1;
-        this.p2 = prop2;
+    constructor({width, length=100}) {
+        this.width = width;
+        this.lenth = length;
     }
 }
 
-const b = new Bar({prop1: 'abc', prop3: 'xyz'});
-
+const b = new Bar({width: 20});
 console.log(b);
+
+
+class ChocolateBar extends Bar {
+    constructor({width, length=100, type}) {
+        super({width, length});  // call superclass constructor
+        this.type = type;
+    }
+}
+
+const cb = new ChocolateBar({width: 2, type: 'milk'});
+console.log(cb);
+
