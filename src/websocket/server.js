e649ad4b6170e5e886b92fed72f9aa01b4f335fb
Bin Le <bin.le.code@gmail.com>
2021-02-13 20:22:52 -0500

    minor addition with socket id

--- src/websocket/server.js
+++ src/websocket/server.js
@@ -1,6 +1,12 @@
 /**
  * 
  * ref: https://socket.io/get-started/chat/
+ * 
+ * To run: 
+ * - node src/websocket/server.js
+ * - go to localhost:3000/index.html and start entering message
+ * - open another webbrowser tab and start entering message
+ * 
  */
 
 
@@ -15,17 +21,21 @@ app.get('/', (req, res) => {
 // refresh the 'index.html' webpage console prints “a user connected”.
 // refresh again closes existing websocket and initialize a new one
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    // socket.broadcast.emit('hello');
+    console.log(`a user (socket ${socket.id}) connected`);
+    // greet the new user
+    socket.emit('chat message', 'welcome!');
+    // broadcast to all other connected sockets except itself
+    socket.broadcast.emit('chat message', `a new user (${socket.id}) connected`);
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log(`user (${socket.id}) disconnected`);
+        socket.broadcast.emit('chat message', `a user (${socket.id}) disconnedted`);
     });
 
     socket.on('chat message', (msg) => {
         console.log('user message: ' + msg);
         // emit event to all connected sockets
-        io.emit('chat message', msg);
+        io.emit('chat message', `user (${socket.id}): ` + msg);
     });
 });
 
